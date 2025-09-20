85dbe688dc3f221a6f39d8cf559fa5216a7e4159
Solomough <Solomough@users.noreply.github.com>
2025-09-20 21:07:09 +0000

    deploy: c49503375ae0c8f40de950b5ca4f2d857ac6e43f

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Replace with your GitHub repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/solomough-portfolio/",
-});
