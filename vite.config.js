618082b903a90de5645bd6cb146d74f94e34a30e
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-20 20:50:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Replace with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-})
+  base: "/solomough-portfolio/",
+});
