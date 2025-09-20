e89f2c22439c6c508f320c83b54d8f6a8ebf69ab
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-20 02:51:15 +0100

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#C7A86A',
+        deepgreen: '#0B6B56'
+      }
+    },
+  },
+  plugins: [],
+}
