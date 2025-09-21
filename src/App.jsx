b20911e205d2eb726e26748dc48e2b7b013b6e1f
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-21 19:48:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+import FaithBooks from "./components/FaithBooks";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -20,3 +23,31 @@ function App() {
       <section id="skills" className="py-20 px-6 md:px-16 bg-white">
         <Skills />
       </section>
+
+      {/* Projects Section */}
+      <section id="projects" className="py-20 px-6 md:px-16 bg-gray-50">
+        <Projects />
+      </section>
+
+      {/* Services Section */}
+      <section id="services" className="py-20 px-6 md:px-16 bg-white">
+        <Services />
+      </section>
+
+      {/* Faith Books Section */}
+      <section id="faithbooks" className="py-20 px-6 md:px-16 bg-gray-50">
+        <FaithBooks />
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact" className="py-20 px-6 md:px-16 bg-white">
+        <Contact />
+      </section>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
