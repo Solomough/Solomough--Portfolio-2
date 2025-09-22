16e978af28a6d915b0ba7dbd329eb9ea129358d8
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-22 22:39:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AnimatePresence mode="wait">
         <App />
       </AnimatePresence>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
